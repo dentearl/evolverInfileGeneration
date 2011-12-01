fc8ac3f5572ab2d986af61a00015170e874d2b15
Dent Earl <dearl@soe.ucsc.edu>
2011-12-01 14:04:32 -0800

    committing a horrible copy-paste job on the verifyPrograms() function rather than trying to tie into the evolverSimControl library. Bad I know, but Im not prepared to tie together these two projects beyond what we already have to deal with.

--- src/trfWrapper.py
+++ src/trfWrapper.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
-simCtrl_wrapperTRF.py, 17 February 2009
+trfWrapper.py, 17 February 2009
  dent earl, dearl (a) soe ucsc edu
 
-simCtrl_wrapperTRF.py takes all the the same arguments that
-trf takes and passes them to trf. wrapper_TRF then checks the
+trfWrapper.py takes all the the same arguments that
+trf takes and passes them to trf. trfWrapper then checks the
 return code and acts appropriately.
 
 trf has issues with return codes and this is a simple way around
@@ -17,33 +17,79 @@ at a time, so we expect a retcode of 1.)
 
 """
 ########################################
-import glob, os, subprocess, sys
-import simulation.lib.libSimControl as LSC
-
-programs = ['trf']
-LSC.verifyPrograms(programs)
-TRF_BIN = programs[0]
+import glob
+import os
+import subprocess 
+import sys
 
 def usage():
     print "USAGE: %s [trf options]" %(sys.argv[0])
     print __doc__
     sys.exit(2)
 
+def verifyPrograms(programs, verbose = False):
+    """verifyPrograms(programs) takes a list of executable names, and acts on the list object
+    to look up the full path to the executables, or if they are not found it raises an exeption
+    """
+    from libSimControl import which
+    if not isinstance(programs, list):
+       raise TypeError('verifyPrograms takes a list of program '
+                       'names, not %s.' % programs.__class__)
+    for i, prog in enumerate(programs, 0):
+       if not isinstance(prog, str):
+          raise TypeError('verifyPrograms list members should all be strings, '
+                          '"%s" not a string, is a %s.' %(str(prog), prog.__class__))
+       p = which(prog)
+       if p is None:
+           raise RuntimeError('verifyPrograms(): Could not locate "%s" '
+                              'in PATH.' % prog)
+       else:
+           if verbose:
+               print '   located executable %s %s OK' % (p, (90 - len(p)) * '.')
+           if prog.endswith('.py') or prog.endswith('.pl') or prog.endswith('.sh'):
+               verifyUnixLineEndings(prog, verbose = verbose)
+           programs[i] = p
+
+def verifyUnixLineEndings(prog, verbose = False):
+    """ verifyUnixLineEndngs takes a program (no path) and checks the line endings to
+    verify that they are unix (LF). If they are not, it raises a RuntimeError.
+    """
+    from libSimControl import which
+    if not isinstance(prog, str):
+        raise TypeError('verifyUnixLineEndings takes a string, not %s.' % prog.__class__)
+    p = which(prog)
+    if p is None:
+        raise RuntimeError('verifyUnixLineEndings(): '
+                           'Could not locate "%s" in PATH' % prog)
+    else:
+        dat = open(p, 'rb').read()
+        if '\0' in dat:
+            # binary
+            return
+        newDat = dat.replace("\r\n", "\n") # replace CRLF with LF
+        if newDat != dat:
+            raise RuntimeError('verifyUnixLineEndings(): %s '
+                               'contains CRLF line endings.' % p)
+        else:
+            if verbose:
+                print '   unix line endings %s %s OK' % (p, (90 - len(p)) * '.')
+
 def main(argv):
-    cmd=[TRF_BIN]
+    programs = ['trf']
+    verifyPrograms(programs)
+    TRF_BIN = programs[0]
+    cmd = [TRF_BIN]
     cmd.extend(argv[1:])
-    localDir=os.path.dirname(argv[1])
-#    sys.stderr.write('I want to run:%s\nfrom this directory:%s\n' %(cmd, localDir))
-#    myDir=glob.glob(os.path.join(localDir,'*'))
-#    sys.stderr.write('contents of that directory: %s\n' %(myDir))
-#    subprocess.Popen('ls -al '+localDir, shell=True)
-    p = subprocess.Popen(cmd, cwd=localDir)
+    localDir = os.path.dirname(argv[1])
+    p = subprocess.Popen(cmd, cwd = localDir)
     p.wait()
     if p.returncode != 1:
         if p.returncode < 0:
-            sys.stderr.write('%s: Experienced an error while trying to execute: %s SIGNAL:%d\n' %(sys.argv[0], ' '.join(cmd), -(p.returncode)))
+            sys.stderr.write('%s: Experienced an error while trying to '
+                             'execute: %s SIGNAL:%d\n' %(sys.argv[0], ' '.join(cmd), -(p.returncode)))
         else:
-            sys.stderr.write('%s: Experienced an error while trying to execute: %s retcode:%d\n' %(sys.argv[0], ' '.join(cmd), p.returncode))
+            sys.stderr.write('%s: Experienced an error while trying to '
+                             'execute: %s retcode:%d\n' %(sys.argv[0], ' '.join(cmd), p.returncode))
         sys.exit(1)
     sys.exit(0)
 
